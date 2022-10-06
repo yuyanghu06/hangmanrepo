81763cfc974674a24079c0fdf4525b38db5c9da4
1911102 <1911102@01-19838.pusd.dom>
2022-10-06 09:03:45 -0700

    testing functions for easy testing

--- HangMan Stu Version/src/HMRunner.java
+++ HangMan Stu Version/src/HMRunner.java
@@ -14,6 +14,14 @@ public class HMRunner {
     
     public static void main(String[] args) {
         HangMan m = new HangMan();
+//        System.out.println(Logic.updateScore(1, true));
+//        System.out.println(Logic.addStrike("asdf", "d"));
+//        System.out.println(Logic.guessWord("asdf", "asdf"));
+//        System.out.println(Logic.letterCheck("asdf", "a"));
+//        System.out.println(Logic.guessLetter("asdf", "asd_", "f"));
+//        System.out.println(Logic.encrypt("asdf", "asd_", "F"));
+//        System.out.println(Logic.withoutSpaces("asdf"));
+//        System.out.println(Logic.withSpaces("asdf"));
         System.out.println(Logic.reset("asdf"));
     }
     
